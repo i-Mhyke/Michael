7aa827dc7c663c4dafef3095ebf6a94dc532965b
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-03-22 18:00:50 +0100

    Create javascript task 1.js
    Startng Javascript task 1

---
+++ javascript task 1.js
@@ -0,0 +1,11 @@
+const name = "Michael Madumere";
+const courses = ['Html', 'Css', 'Javascript', 'Nodejs'];
+
+console.log(name);
+console.log(courses);
+
+for(let i = 1; i <= 200; i++){
+	if (i % 2 === 0){
+		console.log(i);
+	}
+}
